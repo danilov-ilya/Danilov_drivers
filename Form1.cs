5072a3315ce8d21febc7ed573cf2b410ccf10952
danilov-ilya <platon523@gmail.com>
2021-12-06 19:22:34 +0300

    initial commit
    initial commit

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using MassTransit.Monitoring.Performance;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ВодителиВАР3
+{
+    public partial class Form1 : Form
+    {
+        Model1 db = new Model1(); // связываем с БД
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int counter = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter++;
+            if (textBox1.Text == "inspector" || textBox2.Text == "inspector") // проверяет логин и пароль
+            {
+                Form2 db = new Form2();
+                db.Show(); // показывает форму
+                this.Hide(); // скрывает форму Авторизации
+            }
+            else if (counter >= 3) // лимит до 3 попыток
+            {
+                textBox1.Enabled = false; //блокируем текстбокс 1
+                textBox2.Enabled = false; //блокируем текстбокс 2
+                MessageBox.Show("Превышен лимит попыток захода в систему, перезапустите систему");
+            }
+            if (textBox1.Text == "" || textBox2.Text == "") // если пароль и логин незаполнены или неверно заполены
+            {
+                MessageBox.Show("Неверный логин и пароль");
+                return;
+            }
+            else if (counter >= 3) // лимит до 3 попыток
+            {
+                textBox1.Enabled = false; //блокируем текстбокс 1
+                textBox2.Enabled = false; //блокируем текстбокс 2
+                MessageBox.Show("Превышен лимит попыток захода в систему, перезапустите систему");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
